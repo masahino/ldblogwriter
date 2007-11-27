f452defc236c07234d58643f72b05ec378ab63f2
masahino <masahino@0978bef0-6439-0410-a06b-a62e4d60c955>
2007-11-27 12:20:53 +0000

    no comments
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/lbw/ldblogwriter/trunk@16 0978bef0-6439-0410-a06b-a62e4d60c955

--- plugins/hatenagraph.rb
+++ plugins/hatenagraph.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'hatena/api/graph'
+require 'date'
 
 def hatenagraph(graphname, date, value)
   graph = Hatena::API::Graph.new(@conf.options['hatena_username'],
                                  @conf.options['hatena_password'])
-  graph.post_data(graphname, 'date' => date, 'value' => value)
+  graph.post_data(graphname, 'date' => Date.parse(date.to_s).to_s,
+                  'value' => value)
 
   return ""
 end
