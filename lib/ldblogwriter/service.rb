1d0bbe406c16d2f82fb75af3b73ee0bb2a18b952
masahino <masahino@0978bef0-6439-0410-a06b-a62e4d60c955>
2010-03-04 14:59:11 +0000

    none
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/lbw/ldblogwriter/trunk@91 0978bef0-6439-0410-a06b-a62e4d60c955

--- lib/ldblogwriter/service.rb
+++ lib/ldblogwriter/service.rb
@@ -1,30 +1,9 @@
 # -*- coding: utf-8 -*-
 
+require 'ldblogwriter/service/livedoor.rb'
+
 module LDBlogWriter
-  class AbstractService
-    def initialize(config)
-      @config = config
-    end
-
-    def post_entry(content, title, category)
-      raise 'Called abstract method: '+caller.first.scan(/`(.*)'/).to_s
-    end
-
-    def edit_entry(entry)
-      raise 'Called abstract method: '+caller.first.scan(/`(.*)'/).to_s
-    end
-
-    def delete_entry(entry)
-      raise 'Called abstract method: '+caller.first.scan(/`(.*)'/).to_s
-    end
-
-    def post_image(image_file_path)
-      raise 'Called abstract method: '+caller.first.scan(/`(.*)'/).to_s
-    end
-
-    def to_xml(entry)
-      raise 'Called abstract method: '+caller.first.scan(/`(.*)'/).to_s
-    end
+  module Service
 
   end
 end
